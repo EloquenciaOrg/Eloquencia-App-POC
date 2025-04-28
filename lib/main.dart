c940da4269e714e0402ee25b971f9475818f763c
PierreEloquencia <pierre.teles-fache@eloquencia.org>
2025-04-28 17:33:27 +0200

    Added carousel of images and modified buttons and sizedboxes' size

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+const double largebuttonwidth = 250;  // Hauteur des gros boutons
+const double buttonwidth = 150;  // Hauteur des boutons normaux
+const double buttonheight = 10;  // Largeur des boutons normaux
+
+class MyApp extends StatelessWidget {  // L'application
   const MyApp({super.key});
 
   @override
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
           bodyMedium: TextStyle(
             fontSize: 18,
           ),
-        )
+        ),
       )
     );
   }
@@ -32,7 +36,7 @@ class EventPage extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatelessWidget {  // La page d'accueil
   const HomePage({
     super.key,
   });
@@ -40,7 +44,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar(  // Barre de navigation en haut
         backgroundColor: const Color(0xFFFFC107),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -53,11 +57,11 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      endDrawer: Drawer(
+      endDrawer: Drawer(  // Menu de navigation à droite
         child:ListView(
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
+            DrawerHeader(  // En-tête du menu de navigation
               decoration: const BoxDecoration(color: Color(0xFFFFC107)), // quand bouton cliqué : 0xFFFFCA2C
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -140,23 +144,23 @@ class HomePage extends StatelessWidget {
                   width: 350,
                   child: Column(
                     children: [
-                      const SizedBox(height: 70),
-                      const Text('Eloquéncia',
+                      const SizedBox(height: 60),  // Espace entre le haut de la page et le texte
+                      const Text('Eloquéncia',  // Titre de la page d'accueil
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 45,
                         ),
                       ),
-                      const SizedBox(height: 30),
-                      Text('La plateforme de cours en ligne pour apprendre à parler en public',
+                      const SizedBox(height: 40),  // Espace entre le titre et le texte
+                      Text('La plateforme de cours en ligne pour apprendre à parler en public.',  // Slogan de la page d'accueil
                         style: Theme.of(context).textTheme.bodyMedium,
                         textAlign: TextAlign.center,
                       ),
-                      const SizedBox(height: 30),
+                      const SizedBox(height: 20),  // Espace entre le slogan et les boutons d'adhésion et de connexion
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          ElevatedButton(
+                          ElevatedButton(  // Bouton d'adhésion
                             style: ButtonStyle(
                               backgroundColor: const WidgetStatePropertyAll<Color>(Color(0xFFFFC107)),
                               foregroundColor: const WidgetStatePropertyAll<Color>(Colors.black),
@@ -164,6 +168,12 @@ class HomePage extends StatelessWidget {
                                 RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(5),
                                 ),
+                              ),
+                              padding: const WidgetStatePropertyAll<EdgeInsetsGeometry>(
+                                EdgeInsets.symmetric(horizontal: 11),
+                              ),
+                              fixedSize: const WidgetStatePropertyAll<Size>(
+                                Size(buttonwidth, buttonheight),
                               ),
                             ),
 
@@ -177,8 +187,8 @@ class HomePage extends StatelessWidget {
                               style: Theme.of(context).textTheme.bodyMedium
                             ),
                           ),
-                          const SizedBox(width: 10),
-                          ElevatedButton(
+                          const SizedBox(width: 10),  // Espace horizontal entre les deux boutons
+                          ElevatedButton(  // Bouton de connexion
                             style: ButtonStyle(
                               backgroundColor: const WidgetStatePropertyAll<Color>(Color(0xFFFFC107)),
                               foregroundColor: const WidgetStatePropertyAll<Color>(Colors.black),
@@ -190,6 +200,9 @@ class HomePage extends StatelessWidget {
                               padding: const WidgetStatePropertyAll<EdgeInsetsGeometry>(
                                 EdgeInsets.symmetric(horizontal: 11),
                               ),
+                              fixedSize: const WidgetStatePropertyAll<Size>(
+                                Size(buttonwidth, buttonheight),
+                              ),
                             ),
                             
                             onPressed: () {
@@ -198,18 +211,69 @@ class HomePage extends StatelessWidget {
                                 foregroundColor: WidgetStatePropertyAll<Color>(Colors.black),
                               );
                             },
-                            child: Text('Connexion',
+                            child: Text('Se Connecter',
                               style: Theme.of(context).textTheme.bodyMedium
                             ),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 30),
+                    ],
+                  )
+                ),
+                CarouselSlider(  // Carrousel d'images
+                  items: [
+                    Image.asset('assets/images/carousel.jpg',
+                      width: 400,
+                    ),
+                    Image.asset('assets/images/carousel1.jpg',
+                      width: 400,
+                    )
+                  ],
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 7),
+                    enlargeCenterPage: true,
+                    enlargeFactor: 2
+                  )
+                ),
+                SizedBox(
+                  width: 350,
+                  child: Column(
+                    children: [
                       Text('Étudiant·e ou mineur·e ? Vous pouvez faire une demande de réduction ici.',
                         style: Theme.of(context).textTheme.bodyMedium,
                         textAlign: TextAlign.center,
                       ),
-                    ],
+                      const SizedBox(height: 20),  // Espace entre le texte et le bouton de demande de réduction
+                      ElevatedButton(  // Bouton de demande de réduction
+                        // TODO : faire une page de demande de réduction
+                        style: ButtonStyle(
+                          backgroundColor: const WidgetStatePropertyAll<Color>(Color(0xFFFFC107)),
+                          foregroundColor: const WidgetStatePropertyAll<Color>(Colors.black),
+                          shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                          padding: const WidgetStatePropertyAll<EdgeInsetsGeometry>(
+                            EdgeInsets.symmetric(horizontal: 11),
+                          ),
+                          fixedSize: const WidgetStatePropertyAll<Size>(
+                            Size(largebuttonwidth, buttonheight),
+                          ),
+                        ),
+                        
+                        onPressed: () {
+                          const ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll<Color>(Color(0xFFFFCA2C)),
+                            foregroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+                          );
+                        },
+                        child: Text('Demander une réduction',
+                          style: Theme.of(context).textTheme.bodyMedium
+                        ),
+                      ),
+                    ]
                   )
                 )
               ],
